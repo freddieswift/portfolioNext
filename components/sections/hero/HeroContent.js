73bfab7f039d02e25eb44f4e5b037df12754fc22
freddieswift <freddie9117@live.co.uk>
2022-04-28 17:57:44 +0100

    Smaller titles for articles

--- components/sections/hero/HeroContent.js
+++ components/sections/hero/HeroContent.js
@@ -4,7 +4,8 @@ import Button from '../../utils/button/Button'
 const HeroContent = (props) => {
     return (
         <div className={classes.heroContent}>
-            <h1 className='headingPrimary'>{props.heading}</h1>
+            {props.mainHero && <h1 className='headingPrimary'>{props.heading}</h1>}
+            {!props.mainHero && <h1 className={classes.heroTitle}>{props.heading}</h1>}
             <p className={classes.heroDesc}>{props.description}</p>
             {props.mainHero && <Button className='ctaBtn' href='#contactMeSection' btnText='Contact Me'></Button>}
         </div>
